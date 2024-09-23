58f3a49da29687bcc88ec9db51c3786451627128
Jan-Lukas Wynen <jan-lukas.wynen@ess.eu>
2024-09-23 16:34:10 +0200

    Start BIFROST simulation workflow

---
+++ src/ess/bifrost/__init__.py
@@ -0,0 +1,20 @@
+# SPDX-License-Identifier: BSD-3-Clause
+# Copyright (c) 2024 Scipp contributors (https://github.com/scipp)
+# ruff: noqa: E402, F401
+
+"""Components for BIFROST."""
+
+import importlib.metadata
+
+from . import data
+from .io import nexus
+from .workflow import BifrostSimulationWorkflow
+
+try:
+    __version__ = importlib.metadata.version("essspectroscopy")
+except importlib.metadata.PackageNotFoundError:
+    __version__ = "0.0.0"
+
+del importlib
+
+__all__ = ['BifrostSimulationWorkflow', 'data', 'nexus']
