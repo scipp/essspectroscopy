30de06d425b83aceadd2aee6c612f30c37786eff
Jan-Lukas Wynen <jan-lukas.wynen@ess.eu>
2025-04-28 16:29:09 +0200

    Preserve final_energy coord

--- src/ess/spectroscopy/indirect/conversion.py
+++ src/ess/spectroscopy/indirect/conversion.py
@@ -166,6 +166,7 @@ def add_inelastic_coordinates(
             # These are inputs but we want to preserve them
             'a3',
             'a4',
+            'final_energy',  # Needed by SQW
         ],
         graph=graph,
         keep_aliases=False,
